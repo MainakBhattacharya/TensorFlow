b26dff47b9e05002e673bc04aa3689ff9767cfe5
Mainak Bhattacharya <iwc2016005@iiita.ac.in>
2018-03-15 23:40:55 +0530

    added required changes
    added a variable and used it in session

--- tfbasics.py
+++ tfbasics.py
@@ -2,20 +2,23 @@ import tensorflow as tf
 
 x1 = tf.constant(5)
 x2 = tf.constant(6)
-
-result = tf.multiply(x1, x2)
-
-print(result)
+x3 = tf.Variable(x1*2 + x2)
+#print(result)
 
 '''
 sess = tf.Session()
 print(sess.run(result))
 sess.close()
 '''
+
+
 #the following method automatically closes session
 with tf.Session() as sess:
-	output = sess.run(result)
+	sess.run(tf.global_variables_initializer())
+	output = sess.run(x3)
 	#is output a session variable or a python variable??
 	print(output)
 
 print(output)
+#it turns out output is a python variable unrelated to the session itself
+
